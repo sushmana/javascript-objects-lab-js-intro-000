1c0c21e7fac8ee5a059879fe7e36c8e1018fdb9b
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-14 06:30:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var obj = { prop: 1 };
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key]:value;
+  object[key]=value;
   return object;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key]:value;
+  object[key]=value;
   return object;
 }
 
