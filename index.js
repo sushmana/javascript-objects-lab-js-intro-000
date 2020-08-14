9e631872922710515b2105b7460410c834b0a265
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-14 06:27:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var obj = { prop: 1 };
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key]:value;
+  return object;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]:value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
